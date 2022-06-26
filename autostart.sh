54264540d04da07db5f0e7d62c358d3df135587f
clustercode12 <clustercode12@gmail.com>
2022-06-27 01:09:25 +0200

    Patch autostart

---
+++ autostart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Tapping Enabled" 1
+xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Tapping Enabled" 1
+
+xrandr --output eDP-1 --primary --output HDMI-1 --off --output DP-1 --off
+xrandr --output HDMI-1-0 --auto --right-of eDP-1
+
+nitrogen --restore
+megasync
